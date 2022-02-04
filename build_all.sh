06169b54ac1a9c860dfc3c84870af8a65257da7e
supertypo <mthoring@gmail.com>
2022-02-04 23:30:36 +0100

    Update build_all.sh

--- build_all.sh
+++ build_all.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-VERSION=$1
-PUSH=$2
-
 set -e
 
 echo
@@ -10,14 +7,14 @@ echo "========================="
 echo " Building Kaspad"
 echo "========================="
 echo
-./kaspad/build.sh $1 $2
+./kaspad/build.sh $@
 
 echo
 echo "========================="
 echo " Building Loadbalancer"
 echo "========================="
 echo
-./kaspad_loadbalancer/build.sh $1 $2
+./kaspad_loadbalancer/build.sh $@
 
 echo
 echo "========================="
