71ae1ce24117bc3d26fbf9ffa771f74b24070d00
supertypo <mthoring@protonmail.com>
2022-04-05 10:07:09 +0200

    is-synced.sh: Support for v0.11.15. Added support for alternate kaspad-repo (for rc)

--- kaspad/build.sh
+++ kaspad/build.sh
@@ -3,16 +3,17 @@
 VERSION=$1
 KASPAD_VERSION=$(echo $VERSION | grep -oP ".*(?=_.+)")
 PUSH=$2
+REPO=${3:-https://github.com/kaspanet/kaspad}
 
 set -e
 
 if [ -z "$VERSION" -o -z "$KASPAD_VERSION" ]; then
-  echo "Usage ${0} <kaspad-version_buildnr> [push]"
+  echo "Usage ${0} <kaspad-version_buildnr> [push] [kaspad_repo_url]"
   echo "Example: ${0} v0.11.11_1"
   exit 1
 fi
 
-docker build --pull --build-arg KASPAD_VERSION=${KASPAD_VERSION} -t supertypo/kaspad:${VERSION} $(dirname $0)
+docker build --pull --build-arg KASPAD_VERSION=${KASPAD_VERSION} --build-arg REPO=${REPO} -t supertypo/kaspad:${VERSION} $(dirname $0)
 docker tag supertypo/kaspad:${VERSION} supertypo/kaspad:latest
 
 if [ "$PUSH" = "push" ]; then
