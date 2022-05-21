bb817f76eb6e244d56d4741bf5100db04bbdb064
supertypo <mthoring@protonmail.com>
2022-05-21 14:57:27 +0200

    is-synced.sh: Allow connections to diff version hosts

--- kaspad_loadbalancer/is-synced.sh
+++ kaspad_loadbalancer/is-synced.sh
@@ -6,5 +6,5 @@ TIMEOUT_SEC=3
 
 set -e
 
-kaspactl -s $HOST_IP:$HOST_PORT -t $TIMEOUT_SEC GetBlockTemplate kaspa:qpjc562lcm96a0vsx7nn52zrzwvnxfjcs0daf9798wpxjydheke2kt8q65p3s ' ' 2>/dev/null | /bin/grep -qE '"isSynced": *true'
+kaspactl -a -s $HOST_IP:$HOST_PORT -t $TIMEOUT_SEC GetBlockTemplate kaspa:qpjc562lcm96a0vsx7nn52zrzwvnxfjcs0daf9798wpxjydheke2kt8q65p3s ' ' 2>/dev/null | /bin/grep -qE '"isSynced": *true'
 
