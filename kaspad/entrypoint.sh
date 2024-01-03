46dcaaba85ed55ba03746a95879b55fc15163018
marius <suprtypo@pm.me>
2024-01-03 19:10:53 +0100

    Improved entrypoint ipv4/ipv6 support

--- kaspad/entrypoint.sh
+++ kaspad/entrypoint.sh
@@ -1,28 +1,28 @@
 #!/bin/sh
 
-IPV4=$(dig -4 TXT +short +nocomments +timeout=2 +tries=3 o-o.myaddr.l.google.com @ns1.google.com | sed 's/;;.*//' | sed 's/"//g')
-IPV6=$(dig -4 TXT +short +nocomments +timeout=2 +tries=3 o-o.myaddr.l.google.com @ns1.google.com | sed 's/;;.*//' | sed 's/"//g')
-
-if [ "${IPV4}x" != "x" ]; then
-  export EXTERNAL_IP=$IPV4
-elif [ "${IPV6}x" != "x" ]; then
-  export EXTERNAL_IP=$IPV6
-fi
-
-if [ -n "$EXTERNAL_IP" ]; then
-  if echo "$@" | grep -qE "\--listen(=| )"; then
-    port=":$(echo "$@" | grep -oP "\--listen(=| )\S+:\K\d+( |$)" | tail -1)"
-  fi
-  case "$@" in
-    *externalip*) exec dumb-init -- "$@" ;;
-    *)
-      case "$@" in
-        *kaspad*) exec dumb-init -- "$@" --externalip=${EXTERNAL_IP}${port} ;;
-        *) exec dumb-init -- "$@" ;;
-      esac
-    ;;
-  esac
-else
+if echo "$@" | grep -qEv "^(/app/)?kaspad( |$)"; then
   exec dumb-init -- "$@"
+elif echo "$@" | grep -qE "\--externalip(=| )"; then
+  exec dumb-init -- "$@"
+else
+  listenPort=$(echo "$@" | grep -oP "\--listen(=| )\S+:\K\d+( |$)" | tail -1)
+  if [ -n "$listenPort" ]; then
+    listenPort=":$listenPort"
+  fi
+  externalIp4=$(dig -4 TXT +short +nocomments +timeout=2 +tries=3 o-o.myaddr.l.google.com @ns1.google.com | sed 's/;;.*//' | sed 's/"//g')
+  externalIp6=$(dig -6 TXT +short +nocomments +timeout=2 +tries=3 o-o.myaddr.l.google.com @ns1.google.com | sed 's/;;.*//' | sed 's/"//g')
+  if [ -n "$externalIp4" ]; then
+    echo "Public ipv4 address resolved to: $externalIp4"
+    externalIpArgs="--externalip=$externalIp4$listenPort"
+  else
+    echo "Public ipv4 address not found"
+  fi
+  if [ -n "$externalIp6" ]; then
+    echo "Public ipv6 address resolved to: $externalIp6"
+    externalIpArgs="$externalIpArgs --externalip=$externalIp6$listenPort"
+  else
+    echo "Public ipv6 address not found"
+  fi
+  exec dumb-init -- "$@" $externalIpArgs
 fi
 
